bfd55fcc0572e83d53d1fa0f95c48466839a23c7
locus <you@example.com>
2025-05-16 13:33:19 +0000

    updated version

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 family = ['damian','obed','jessie','ahmed']
 print("this is a list of the people i have invited for dinner " + str(family))
 print("Can you bring my cat back Mr. " + family[0])
-print("Tell my son " + family[1] + " to hold onto his")
+print("Tell my son " + family[1] + " to hold onto his bear")
 print(family[2] + " is the son of Mr. David")
 print(family[3] + " was my bestfriend when he was alive")
 print("I have found a bigger table")
@@ -22,7 +22,10 @@ family.append('rodney')
 
 #Printing a new set of invitation messages
 print("Hello " + family[-1] + " please honor my invitation to dinner with the boys")
-print("My dear bother" + family[0] + "it has been a while")
+print("My dear bother " + family[0] + " it has been a while")
+print("I invite you to dinner with me brother " + family[1])
+print("It is a pleasure to dine with you, " + family[2])
+print("cannot wait to see you " + family[3])
 
 
 
