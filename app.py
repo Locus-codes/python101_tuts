b25b39bca8184c0c9f1de369eeb0cb54884c22a0
locus <you@example.com>
2025-05-16 13:53:50 +0000

    finaly

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ print(family)
 family.insert(0,'joe')
 family.insert(3,'russel')
 family.append('rodney')
+print("New list: " + str(family))
 
 #Printing a new set of invitation messages
 print("Hello " + family[-1] + " please honor my invitation to dinner with the boys")
@@ -27,7 +28,20 @@ print("I invite you to dinner with me brother " + family[1])
 print("It is a pleasure to dine with you, " + family[2])
 print("cannot wait to see you " + family[3])
 
+#Using the pop statement to delete list items
+popped_fam1 = family.pop()
+print("I'm sorry i cannot have you for dinner " + popped_fam1)
 
+popped_fam2 = family.pop(1)
+print("Can we make it another time " + popped_fam2)
+
+#removing list items using the del statement
+del family[0]
+del family[1]
+del family[2]
+del family[0]
+del family[0]
+print("The end " + str(family))
 
 
 
