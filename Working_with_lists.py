109de3a53251873c5d66234d7cb70040b1b8a941
locus <you@example.com>
2025-05-19 20:49:32 +0000

    for loops

---
+++ Working_with_lists.py
@@ -0,0 +1,16 @@
+pizzas = ['pepperoni','barbecue','pineapple','vegetable']
+
+for pizza in pizzas:
+    print("I like " + pizza + " pizza!" )
+
+print("\n" + pizzas[2] + " is my favorite.")
+print(pizzas[1] + " is second on the list.")
+print("\nI really love pizza!")
+
+
+animals = ['dogs','horses','cats']
+
+for animal in animals:
+    print("I love " + animal)
+
+print("\nAll these animals have 4 limbs.")
