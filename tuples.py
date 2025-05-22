15c26a2897d7937b550408e25b51282f47d4b2a0
locus <you@example.com>
2025-05-22 07:08:41 +0000

    Intoduction to tuples

---
+++ tuples.py
@@ -0,0 +1,24 @@
+# Introduction to tuples - immutable lists
+dimensions = (200,300)
+print("This is the original version: ")
+print(dimensions)
+
+dimensions = (5,9)
+print("This a modified version: ")
+print(dimensions)
+
+# tuple implementation
+foods = ('rice','fufu','porridge','pasta','pizza')
+
+print("This is the original tuple")
+# Using for loop to print the elements of a tuple 
+for food in foods:
+    print(food)
+print("\n")
+
+# Writing over the tuple since tuples are immutable
+foods = ('rice','chip','burger','pasta','pizza')
+
+print("This is the modified tuple")
+for food in foods:
+    print(food)
